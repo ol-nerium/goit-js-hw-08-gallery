0a741aacbb5bbc1b6df90f556bea19677a0629a7
Xander <76491243+ol-nerium@users.noreply.github.com>
2025-12-26 20:14:29 +0200

    return previous link in static data file

--- js/data.js
+++ js/data.js
@@ -1,10 +1,10 @@
 export const galleryItems = [
   {
     preview:
-      "https://pixabay.com/get/ge6ae7cc743bb782f19eb8f2a3052575066f88c5835d2e9a80e1dd91327f3a4be3ab09ba99e4c6b994f8f76a289e4c67509b089b440f44b19f5f10dbd470f08908f15ae4a0e2a55f4db61bf9ccdc0b3f9_640.jpg",
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
     original:
-      "https://cdn.pixabay.com/photo/2020/06/25/03/20/cherryblossom-5338324_1280.jpg",
-    description: "Cherry Blossom",
+      'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+    description: 'Hokkaido Flower',
   },
   {
     preview:
